b3283cf7234055bde89893a60b60edb98bbdd97b
godric4 <gmarto400@gmail.com>
2023-05-24 09:45:47 +0100

    updated data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -14,7 +14,7 @@ export const items = [
  {
   id: 2,
   name: 'Targets',
-  category: 'REACT',
+  category: 'React',
   field: 'To-Do',
   description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem voluptates enim magnam ducimus eius voluptatibus quo eveniet vero necessitatibus libero officiis provident, temporibus vitae cupiditate non omnis recusandae delectus incidunt?',
   imgUrl: 'https://res.cloudinary.com/gentlegiant/image/upload/v1675536347/samples/screenshots/targets_ocw431.png',
