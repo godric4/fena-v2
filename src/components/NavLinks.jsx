fb2942e9e75691bf27b6723ca944d8780cb55029
godric4 <gmarto400@gmail.com>
2023-08-05 17:25:54 +0100

    updates navbar codes

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,59 +1,53 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import { links } from '../utils/data';
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { links } from '../utils/data'
 
 const NavLinks = () => {
- return (
-  <Wrapper>
-  <ul className="nav-links">
-     {
-  links.map((link) => {
-   const {id, name, path} = link
-  return <li className="link" key={id}>
-   <Link to={path}>{name} </Link>
-  </li>
-  })
- }
-  </ul>
-  </Wrapper>
- )
+  return (
+    <Wrapper>
+      <ul className='nav-links'>
+        {links.map((link) => {
+          const { id, name, path } = link
+          return (
+            <li className='link' key={id}>
+              <Link to={path}>{name} </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </Wrapper>
+  )
 }
 
-const Wrapper = styled.div `
- transition: var(--transition);
- font-size: 16px;
+const Wrapper = styled.div`
+  transition: var(--transition);
+  font-size: 16px;
 
- a:active {
-   color: chocolate;
-   transition: var(--transition);
- }
+  a:active {
+    color: chocolate;
+    transition: var(--transition);
+  }
 
-    a:hover {
+  a:hover {
     color: chocolate;
     font-weight: 400;
     transition: var(--transition);
-
-   }   
-
-
- .nav-links {
-  display: flex;
-  margin-left: -3rem;
-
-
-
-  li {
-   list-style-type: none;
-   margin: 1rem;
   }
- }
 
+  .nav-links {
+    display: flex;
+    margin-left: 25rem;
 
- @media screen and (max-width: 992px) {
- display: none;
- }
+    li {
+      list-style-type: none;
+      margin: 1rem;
+    }
+  }
 
+  @media screen and (max-width: 992px) {
+    display: none;
+  }
 `
 
 export default NavLinks
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,62 +5,58 @@ import { Link } from 'react-router-dom'
 import { FaBars } from 'react-icons/fa'
 import Sidebar from './SideBar'
 
-
 const Navbar = () => {
- 
   return (
-    <Wrapper >
-     <div className="container">
-    <div className="name">
-    <Link to="/">  Godric Marto</Link>
-     </div>
-     <div className="">
-      <NavLinks />
-     </div>
-     <Sidebar />
-     </div>   
+    <Wrapper>
+      <div className='container'>
+        <div className='name'>
+          <Link to='/'> Godric Marto</Link>
+        </div>
+        <div className=''>
+          <NavLinks />
+        </div>
+        <Sidebar />
+      </div>
     </Wrapper>
   )
 }
 
-const Wrapper = styled.nav `
+const Wrapper = styled.nav`
   background: rgba(5, 56, 107, 0.9);
- position: fixed;
+  position: fixed;
   top: 0;
   width: 100%;
   z-index: 199;
- .container {
-  display: flex;
- justify-content: space-between; 
- height: 4rem;
- color: white;
- transition: var(--transition);
- }
+  .container {
+    display: flex;
+    justify-content: space-between;
+    height: 4rem;
+    color: white;
+    transition: var(--transition);
+  }
 
- .name {
-  color: white;
-  font-size: 38px;
+  .name {
+    color: white;
+    font-size: 38px;
     font-weight: 700;
     font-family: 'lobster';
- }
- 
+  }
 
- .nav-toggle {
-  background: transparent;
- margin-top: -.5rem;
+  .nav-toggle {
+    background: transparent;
+    margin-top: -0.5rem;
 
-  color: white;
-  border: none;
-  font-size: 2rem;
-  display: none;
- }
+    color: white;
+    border: none;
+    font-size: 2rem;
+    display: none;
+  }
 
-@media screen and (max-width: 992px) {
- .nav-toggle {
-  display: block;
- }
- }
-
+  @media screen and (max-width: 992px) {
+    .nav-toggle {
+      display: block;
+    }
+  }
 `
 
 export default Navbar
