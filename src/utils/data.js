f695b9bd48bba64cb5a0c61efbcd211d053676cc
Godric Marto <111025788+godric4@users.noreply.github.com>
2025-02-22 19:23:58 +0100

    Update data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -21,7 +21,7 @@ export const items = [
     description:
       "A sleek and modern bar & lounge website designed for an immersive customer experience. Built with a responsive UI, smooth navigation, and engaging visuals to showcase the ambience, menu, and events effortlessly",
     imgUrl:
-      'https:'https://res.cloudinary.com/gentlegiant/image/upload/v1740245537/barlounge_pv64wb.png',
+      'https:https://res.cloudinary.com/gentlegiant/image/upload/v1740245537/barlounge_pv64wb.png',
     link: 'https://godrichelounge.netlify.app/',
   },
   {
